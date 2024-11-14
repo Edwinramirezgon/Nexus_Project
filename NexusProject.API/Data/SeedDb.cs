3a37ca0e7035ae0e242d82e1ef9c1e2fa25ab1d0
edwin <edwin@DESKTOP-CCMSVUV>
2024-11-14 15:17:07 -0500

    Final

--- NexusProject.API/Data/SeedDb.cs
+++ NexusProject.API/Data/SeedDb.cs
@@ -27,10 +27,10 @@ namespace NexusProject.API.Data
             await CheckRoleAsync();
             await CheckUserAsync("98764597", "Edwin", "Ramirez", "EDWINRAMIREZGON@GMAIL.COM", UserType.Admin, "https://nexusprojectitm.s3.us-east-2.amazonaws.com/08956e0c-da87-404d-9877-2144615a3ffc.jpg");
             await CheckUserAsync("101715698", "Juan David", "Velasquez", "JUANDAV12@GMAIL.COM", UserType.Admin, "https://nexusprojectitm.s3.us-east-2.amazonaws.com/2d467b7f-b4e3-42c5-b035-8524a1b7841f.jpg");
-            await CheckUserAsync("98764596", "Tutor", "Number1", "Tutor1@Nexus.com", UserType.Tutor, "https://nexusprojectitm.s3.us-east-2.amazonaws.com/08956e0c-da87-404d-9877-2144615a3ffc.jpg");
-            await CheckUserAsync("98764595", "Young", "Number1", "Young1@Nexus.com", UserType.Young, "https://nexusprojectitm.s3.us-east-2.amazonaws.com/08956e0c-da87-404d-9877-2144615a3ffc.jpg");
-            await CheckUserAsync("98764594", "Young", "Number2", "Young2@Nexus.com", UserType.Young, "https://nexusprojectitm.s3.us-east-2.amazonaws.com/08956e0c-da87-404d-9877-2144615a3ffc.jpg");
-            await CheckUserAsync("98764593", "Tutor", "Number2", "Tutor2@Nexus.com", UserType.Tutor, "https://nexusprojectitm.s3.us-east-2.amazonaws.com/08956e0c-da87-404d-9877-2144615a3ffc.jpg");
+            await CheckUserAsync("98764596", "Math", "Tutor", "Tutor1@Nexus.com", UserType.Tutor, "");
+            await CheckUserAsync("98764595", "Paula", "young", "Young1@Nexus.com", UserType.Young, "");
+            await CheckUserAsync("98764594", "Robin", "Young", "Young2@Nexus.com", UserType.Young, "");
+            await CheckUserAsync("98764593", "Science", "Tutor", "Tutor2@Nexus.com", UserType.Tutor, "");
             await CheckYoungAsync();
             await CheckTutorAsync();
             await CheckAdminAsync();
