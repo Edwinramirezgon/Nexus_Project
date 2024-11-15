6445ae597dbbd460782d28eefb169c68370aaa9d
edwin <edwin@DESKTOP-CCMSVUV>
2024-11-14 20:45:55 -0500

    foreign keys

--- NexusProject.Shared/Entities/foundation.cs
+++ NexusProject.Shared/Entities/foundation.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace NexusProject.Shared.Entities
@@ -27,5 +29,16 @@ namespace NexusProject.Shared.Entities
         [Required(ErrorMessage = "The field {0} is mandatory")]
         [MaxLength(400, ErrorMessage = "More than 400 charachters are not allowed")]
         public string Proyects { get; set; }
+
+        [ForeignKey("YoungId")]
+        [JsonIgnore]
+        public Young young { get; set; }
+        public int YoungId { get; set; }
+
+
+        [ForeignKey("ActivityId")]
+        [JsonIgnore]
+        public Activity activity { get; set; }
+        public int ActivityId { get; set; }
     }
 }
